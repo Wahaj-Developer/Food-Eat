7a3149fc3a9dbbe8f1cff27a4b5391702018ff08
Web-Code <ahmedwahaj317@gmail.com>
2025-06-12 11:08:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const ham = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav-links');
+
+ham.addEventListener('click', () => {
+
+    nav.classList.toggle('active');
+
+
+});
+
 
